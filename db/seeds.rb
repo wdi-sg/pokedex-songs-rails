c370ace65e1c7462f98007943169e6eff3a1710b
JulianPoh <julianpoh01@gmail.com>
2018-07-18 16:47:55 +0800

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-#clear the table before we seed
-Pokemon.destroy_all
-
-json = ActiveSupport::JSON.decode(File.read('db/pokedex.json'))
-
-json['pokemon'].each do |item|
-  new_pokemon = Pokemon.new
-  new_pokemon.num = item['num']
-  new_pokemon.name = item['name']
-  new_pokemon.img = item['img']
-  new_pokemon.height = item['height']
-  new_pokemon.weight = item['weight']
-  new_pokemon.candy = item['candy']
-  new_pokemon.candy_count = item['candy_count']
-  new_pokemon.egg = item['egg']
-  new_pokemon.avg_spawns = item['avg_spawns']
-  new_pokemon.spawn_time = item['spawn_time']
-  new_pokemon.save
-end
