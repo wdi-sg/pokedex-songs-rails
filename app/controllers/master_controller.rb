26b2cbda2328d5627435cc7b107d9b8a3018f270
Phangster <mynamebryanphang@gmail.com>
2018-07-19 15:52:49 +0800

    Completed

---
+++ app/controllers/master_controller.rb
@@ -0,0 +1,55 @@
+class MasterController < ApplicationController
+
+  def index
+    @masters = Master.all
+  end
+
+  def new
+    @pokemons = Pokemon.all
+    #renders new.html.erb in views folder because they have the same name
+  end
+  
+  def show
+    @masters = Master.find(params[:id])
+    #cases when trying to get a master for the pokemon that does not exist
+    
+    if(parmams[:pokemon_id].to_i != @master.pokemon.id)
+      puts "master does not exists"
+    end
+  end
+
+  def create
+    @master = Master.new(master_params)
+
+    @master.save
+    redirect_to master_index_path
+  end
+
+  def update
+    @master = Master.find(params[:id])
+
+    @master.update(master_params)
+    redirect_to master_index_path
+
+  end
+
+  def edit
+    @master = Master.find(params[:id])
+    @pokemons = Pokemon.all
+
+  end
+
+  def destroy
+    @master = Master.find(params[:id])
+
+    @master.destroy
+    redirect_to master_index_path
+  end
+
+  private
+
+  def master_params
+    params.require(:master).permit(:name, :pokemon_id)
+  end
+
+end
--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,74 +1,47 @@
 class PokemonController < ApplicationController
 
-    def index
-      #this instance variable "SELECT "pokemons".* FROM "pokemons"
-      @pokemons = Pokemon.all
-    end
-  
-    def show
-      #  SELECT  "pokemons".* FROM "pokemons" WHERE "pokemons"."id" = $1
-      @pokemon = Pokemon.find(params[:id])
-    end
-    
-    def new
-    #renders new.html.erb in views folder because they have the same name
-    end
-  
-    def create
-      @pokemons = Pokemon.new(pokemon_params)
-  
-      @pokemons.save
-      redirect_to pokemon_index_path
-    end
-  
-    def update
-      @pokemon = Pokemon.find(params[:id])
-      @pokemon.update(pokemon_params)
-      redirect_to pokemon_index_path
-    end 
-    
-    def edit
-      @pokemon = Pokemon.find(params[:id])
-    end
-  
-    def destroy
-      @pokemon = Pokemon.find(params[:id])
-  
-      @pokemon.destroy
-      redirect_to pokemon_index_path
-    end
-  
-    private
-  
-    def pokemon_params
-      params.require(:pokemon).permit(:num, :name, :img, :height, :weight, :candy, :candy_count, :egg, :avg_spawns, :spawn_time)
-    end
-  
+  def index
+    #this instance variable "SELECT "pokemons".* FROM "pokemons"
+    @pokemons = Pokemon.all
   end
 
-  class MasterController < ApplicationController
-    def index
-    end
-
-    def show
-    end
-
-    def create
-    end
-
-    def update
-    end
-
-    def edit
-    end
-
-    def destroy
-    end
-
-    private
-
-    def master_parmas
-        parmas.require(:master).permit(name:, pokemon_id:)
-    end
-    
+  def show
+    #  SELECT  "pokemons".* FROM "pokemons" WHERE "pokemons"."id" = $1
+    @pokemon = Pokemon.find(params[:id])
   end
+  
+  def new
+  #renders new.html.erb in views folder because they have the same name
+  end
+
+  def create
+    @pokemons = Pokemon.new(pokemon_params)
+
+    @pokemons.save
+    redirect_to pokemon_index_path
+  end
+
+  def update
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.update(pokemon_params)
+    redirect_to pokemon_index_path
+  end 
+  
+  def edit
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+  def destroy
+    @pokemon = Pokemon.find(params[:id])
+
+    @pokemon.destroy
+    redirect_to pokemon_index_path
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:num, :name, :img, :height, :weight, :candy, :candy_count, :egg, :avg_spawns, :spawn_time)
+  end
+
+end
